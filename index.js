f1e82c49823dd81cde3e88d88b99a8e2707ea3d2
jacopofar <jacopo1.farina@gmail.com>
2015-12-04 11:52:43 +0100

    more details and defaults from config file

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ var irc = require('irc');
 var nconf = require('nconf');
 var path = require('path');
 
-nconf.argv()
+  nconf.argv()
 .env()
-.file({ file: 'config.json' })
-.defaults({script_directory:'scripts'});
+  .file({ file: 'config.json' })
+  .defaults({script_directory:'scripts',
+    sasl:false,
+    irc_server_password:'',
+    irc_realname:'ircbotjs',
+    irc_username:''
+  });
 
 var client = new irc.Client(nconf.get('irc_server'), nconf.get('irc_nickname'), {
-
-	userName: nconf.get('irc_username'),
-	realName: nconf.get('irc_realname'),
-	port: nconf.get('irc_server_port'),
-	channels: nconf.get('irc_channels'),
-	sasl: nconf.get('sasl'),
-	password: nconf.get('irc_server_password'),
+  userName: nconf.get('irc_username'),
+  realName: nconf.get('irc_realname'),
+  port: nconf.get('irc_server_port'),
+  channels: nconf.get('irc_channels'),
+  sasl: nconf.get('sasl'),
+  password: nconf.get('irc_server_password'),
 });
 
 fs.readdir(nconf.get('script_directory'),function(err,files){
