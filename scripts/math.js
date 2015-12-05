0b5adac0f7b83caf8c122fa0ac521bc3d6fec8da
jacopofar <jacopo1.farina@gmail.com>
2015-12-05 22:10:00 +0100

    added math script to perform calculations

---
+++ scripts/math.js
@@ -0,0 +1,27 @@
+'use strict';
+var mathjs = require('mathjs');
+
+module.exports.description = "Calculate arithmetical expressions using math.js";
+
+module.exports.main = function(client){
+  client.addListener('message#', function(nick,to,text,message) {
+    if(text.indexOf('!calc') === 0){
+      if(text.toLowerCase().indexOf('the answer') !== -1 && text.indexOf('42') === -1){
+        client.say(to,nick+": "+42);
+        return;
+      }
+      var expr = text.replace('!calc','').trim();
+      if(expr === ''){
+        client.say(to,nick+": usage !calc <expression>, expressions are the ones accepted by math.js");
+        return;
+      }
+      try{
+        var result = mathjs.eval(expr);
+        client.say(to,nick+": "+result);
+      }
+      catch(e){
+        client.say(to,nick+": error - "+e); 
+      }
+    }
+  });
+};
