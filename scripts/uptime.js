b60b14b4201133d1750abe71b3297242656199e0
jacopofar <jacopo1.farina@gmail.com>
2015-12-05 22:47:42 +0100

    more accurate uptime message

--- scripts/uptime.js
+++ scripts/uptime.js
@@ -4,7 +4,7 @@ var moment = require('moment')
 module.exports.main = function(client){
   client.addListener('message', function(nick,to,text,message) {
     if(text === '!uptime'){
-      client.say(to,"bot process started "+moment(new Date(new Date()-process.uptime()*1000)).fromNow()+", system running from "+moment(new Date(new Date()-os.uptime()*1000)).fromNow());
+      client.say(to,"bot process started "+moment(new Date(new Date()-process.uptime()*1000)).fromNow()+", system booted "+moment(new Date(new Date()-os.uptime()*1000)).fromNow());
     }
   });
 };
